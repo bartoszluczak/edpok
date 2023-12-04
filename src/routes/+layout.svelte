c9df34931e2ab5b02068fdc81a88d0843e1d7e94
bluczak <bartosz@zazuafrica.com>
2023-12-04 18:04:40 +0100

    Add analitics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
 	import { webVitals } from '$lib/vitals';
 	import Header from './Header.svelte';
 	import './styles.css';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 
 	/** @type {import('./$types').LayoutServerData} */
 	export let data;
